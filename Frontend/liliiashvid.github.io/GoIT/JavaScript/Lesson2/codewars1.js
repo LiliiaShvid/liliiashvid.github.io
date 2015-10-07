37a543d1676d50088c8ee234f04a7771a03223c6
LiliiaShvid <liliia.shvid@gmail.com>
2015-10-07 18:34:30 +0200

    js lesson1, 2

---
+++ Frontend/liliiashvid.github.io/GoIT/JavaScript/Lesson2/codewars1.js
@@ -0,0 +1,8 @@
+/**
+ * Created by User on 07.10.2015.
+ */
+
+function numberToString(num) {
+    // Return a string of the number here!
+    return num + '';
+}
---
+++ Frontend/liliiashvid.github.io/GoIT/JavaScript/Lesson2/codewars2.js
@@ -0,0 +1,6 @@
+/**
+ * Created by User on 07.10.2015.
+ */
+var stringToNumber = function(str){
+    return +str;
+}
